719fadf7211f9dd0437948a651728be3e5c6faf8
Prohit <udkarprohit@gmail.com>
2023-02-06 09:26:55 +0530

    Added: Validating Last Name entered by the user

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,21 +4,35 @@ import java.util.Scanner;
 
 public class UserRegistration {
 
-    // validate first name
+    // Validate first name
     public static boolean validateFirstName(String firstName) {
-        return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
+        return firstName.matches("^[A-Z][a-z]{2,}$");
     }
+    // Validate last name
+    public static boolean validateLastName(String lastName) {
+        return lastName.matches("^[A-Z][a-z]{2,}$");
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        //          First Name
         System.out.print("Enter The First Name : ");
         String firstName = scanner.nextLine();
-
+        //Checking for valid first name
         if (!validateFirstName(firstName)) {
             System.out.println("Invalid First Name");
         } else {
-            System.out.println("First Name : " + firstName);
+            System.out.println("First Name " + firstName);
         }
-
+        //          Last Name
+        System.out.print("Enter The Last Name : ");
+        String lastName = scanner.nextLine();
+        //Checking for valid last name
+        if (!validateLastName(lastName)) {
+            System.out.println("Invalid Last Name");
+        } else {
+            System.out.println("Last Name " + lastName);
+        }
     }
 }
