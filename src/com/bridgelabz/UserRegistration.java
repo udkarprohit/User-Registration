6353d146451c41f74eeb014787ae1fa8ae12924e
Prohit <udkarprohit@gmail.com>
2023-02-12 03:47:39 +0530

    Added: Useing Lambda Function to validate User Registration

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,102 +1,67 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class UserRegistration {
-    // Validate first name
-    public static void validateFirstName(String firstName) throws InvalidDetailsException {
-//        return firstName.matches("^[A-Z][a-z]{2,}$");
-        if (!firstName.matches("^[A-Z][a-z]{2,}$")) {
-            throw new InvalidDetailsException("Invalid first name");
-        }
-    }
 
-    // Validate last name
-    public static void validateLastName(String lastName) throws InvalidDetailsException {
-//        return lastName.matches("^[A-Z][a-z]{2,}$");
-        if (!lastName.matches("^[A-Z][a-z]{2,}$")) {
-            throw new InvalidDetailsException("Invalid last name");
-        }
-    }
+    static Scanner scanner = new Scanner(System.in);
 
-    // Validating User Email
-    public static void validateUserEmail(String userEmail) throws InvalidDetailsException {
-//        return userEmail.matches("^[a-z]{3,}[-_+.a-z0-9]*[@][0-1a-z]+[.][a-z]{2,}[.]?[,a-z]{0,3}$");
-        if (!userEmail.matches("^[a-z]{3,}[-_+.a-z0-9]*[@][0-1a-z]+[.][a-z]{2,}[.]?[,a-z]{0,3}$")) {
-            throw new InvalidDetailsException("Invalid email");
-        }
-    }
+    // Predicate to validate first name
+    static Predicate<String> validateFirstName = firstName -> firstName.matches("^[A-Z][a-z]{2,}$");
+    // Predicate to validate last name
+    static Predicate<String> validateLastName = lastName -> lastName.matches("^[A-Z][a-z]{2,}$");
+    // Predicate to validate user email
+    static Predicate<String> validateUserEmail = userEmail -> userEmail.matches("^[a-z]{3,}[-_+.a-z0-9]*[@][0-1a-z]+[.][a-z]{2,}[.]?[,a-z]{0,3}$");
+    // Predicate to validate user phone number
+    static Predicate<String> validatePhoneNumber = phoneNumber -> phoneNumber.matches("^[+](91)[\\s][0-9]{10}$");
+    // Predicate to validate user password
+    static Predicate<String> validateUserPassword = userPassword -> userPassword.matches("^[A-Z][a-z]{7,}[0-9]+[@#$%&]$");
 
-    // Validate User Phone Number
-    public static void validatePhoneNumber(String phoneNumber) throws InvalidDetailsException {
-//        return phoneNumber.matches("^[+](91)[\\s][0-9]{10}$");
-        if (!phoneNumber.matches("^[+](91)[\\s][0-9]{10}$")) {
-            throw new InvalidDetailsException("Invalid phone number");
-        }
-    }
-
-    // Validate User Password
-    public static void validateUserPassword(String userPassword) throws InvalidDetailsException {
-//        return userPassword.matches("^[A-Z][a-z]{7,}[0-9]+[@#$%&]$");
-        if (!userPassword.matches("^[A-Z][a-z]{7,}[0-9]+[@#$%&]$")) {
-            throw new InvalidDetailsException("Invalid password");
-        }
-    }
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-//                  First Name
-        try {
-            System.out.print("Enter The First Name : ");
-            String firstName = scanner.nextLine();
-            //Checking for valid first name
-            validateFirstName(firstName);
+        // First Name
+        System.out.print("Enter The First Name : ");
+        String firstName = scanner.nextLine();
+        if (!validateFirstName.test(firstName)) {
+            System.out.println("Invalid First Name");
+        } else {
             System.out.println("First Name: " + firstName);
-        } catch (InvalidDetailsException e) {
-            System.out.println(e.getMessage());
         }
-        try {
-            //          Last Name
-            System.out.print("Enter The Last Name : ");
-            String lastName = scanner.nextLine();
-            //Checking for valid last name
-            validateLastName(lastName);
-            System.out.println("Last Name : " + lastName);
-        } catch (InvalidDetailsException e) {
-            System.out.println(e.getMessage());
+        // Last Name
+        System.out.print("Enter The Last Name : ");
+        String lastName = scanner.nextLine();
+        if (!validateLastName.test(lastName)) {
+            System.out.println("Invalid Last Name");
+        } else {
+            System.out.println("Last Name: " + lastName);
         }
-
-            //          User Email
-        try {
-            System.out.print("Enter The User Email : ");
-            String userEmail = scanner.nextLine();
-            //Checking for valid Email
-            validateUserEmail(userEmail);
-            System.out.println("User Email " + userEmail);
-        } catch (InvalidDetailsException e) {
-            System.out.println(e.getMessage());
+        //User Email
+        System.out.print("Enter The User Email : ");
+        String userEmail = scanner.nextLine();
+        if (!validateUserEmail.test(userEmail)) {
+            System.out.println("Invalid User Email");
+        } else {
+            System.out.println("User Email: " + userEmail);
         }
 
-            //          User Phone Number
-        try {
-            System.out.print("Enter The User Phone Number : ");
-            String phoneNumber = scanner.nextLine();
-            //Checking for valid Phone Number
-            validatePhoneNumber(phoneNumber);
-            System.out.println(" User Phone Number " + phoneNumber);
-        } catch (InvalidDetailsException e) {
-            System.out.println(e.getMessage());
+        // User Phone Number
+        System.out.print("Enter The User Phone Number : ");
+        String phoneNumber = scanner.nextLine();
+        if (!validatePhoneNumber.test(phoneNumber)) {
+            System.out.println("Invalid User Phone Number");
+        } else {
+            System.out.println("User Phone Number: " + phoneNumber);
         }
-//                  User Password
-        try {
-            System.out.print("Enter The User Password : ");
-            String userPassword = scanner.nextLine();
-            //Checking for valid Phone Number
-            validateUserPassword(userPassword);
-            System.out.println("User Password " + userPassword);
-        } catch (InvalidDetailsException e) {
-            System.out.println(e.getMessage());
+
+        // User Password
+        System.out.print("Enter The User Password : ");
+        String userPassword = scanner.nextLine();
+        if (!validateUserPassword.test(userPassword)) {
+            System.out.println("Invalid Password");
+        } else {
+            System.out.println("User Password : *************");
         }
+
     }
 }
